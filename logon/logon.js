e5d1d9bb3631e4008ae1482cde6f72c2eaed412a
Jason M <jason@baydq.com>
2021-07-29 14:39:52 -0500

    Add custom alerts to sign-in page

--- logon/logon.js
+++ logon/logon.js
@@ -1,5 +1,13 @@
 const socket = io();
 
+let alert_countdown;
+window.alert = (text) => {
+    document.getElementById("alert").innerText = text;
+    alert_countdown = setTimeout(() => {
+        document.getElementById("alert").innerText = ""
+    }, 5000);
+}
+
 function setCookie(cname, cvalue, exdays) {
     let exdate = new Date();
     exdate.setTime(exdate.getTime() + (exdays*24*60*60*1000));
@@ -9,13 +17,15 @@ function setCookie(cname, cvalue, exdays) {
 
 document.getElementById("emailform").addEventListener('submit', (event)=>{
     event.preventDefault()
-    document.getElementById('submit').remove()
+    document.getElementById('submit').style.display = 'none'
     document.getElementById('pleasewait').style.display = 'initial'
     const formdata = new FormData(document.getElementById("emailform"))
     socket.emit('email-sign-in', formdata.get('email'), (response) => {
         switch (response) {
             case "bad_email":
                 alert('The email you entered is not on our database. Please check for typos and try again using all lowercase letters, and if that does not work, contact me.');
+                document.getElementById('submit').style.display = 'initial'
+                document.getElementById('pleasewait').style.display = 'none'
                 break;
             case "sent":
                 alert('Email sent. Check your inbox for an email from chat.email.wfb@gmail.com. It may be in your Spam folder, so make sure to check there if you don\'t find it.')
@@ -27,6 +37,7 @@ document.getElementById("emailform").addEventListener('submit', (event)=>{
                     socket.emit('confirm-code', Cformdata.get('confirm'), (response)=>{
                         switch (response.status) {
                             case "auth_done":
+                                alert("Authentication succeeded. You will be redirected soon.")
                                 setCookie('name', response.data.name, 30)
                                 setCookie('cdid', response.data.cdid, 30)
                                 setCookie('authname', response.data.authname, 30)
@@ -34,7 +45,7 @@ document.getElementById("emailform").addEventListener('submit', (event)=>{
                                 break;
                             case "auth_failed":
                             default:
-                                alert("Authentication Failed")
+                                alert("Authentication Failed.")
                                 window.location.reload()
                                 break;
                         }
@@ -44,6 +55,8 @@ document.getElementById("emailform").addEventListener('submit', (event)=>{
             case "send_err":
             default:
                 alert('An unexpected error occurred during the sending of your email. Please try again and contact me if this persists.')
+                document.getElementById('submit').style.display = 'initial'
+                document.getElementById('pleasewait').style.display = 'none'
                 break;
         }
     })
