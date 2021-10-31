9f56f9f73c744eaef4e13cbcc72692d8b8023ae1
one23four56 <jason@baydq.com>
2021-10-30 20:29:27 -0500

    Add css to update logs page

--- index.ts
+++ index.ts
@@ -318,10 +318,10 @@ app.use((req, res, next) => {
   })
   app.get("/updates", (req, res)=>{
     let response = `<head><title>Backup Google Chat Update Logs</title>`
-    response += `<style>* {font-family:monospace}</style></head><h1>Backup Google Chat Update Logs</h1><ul>`
+    response += `<style>li {font-family:monospace} h1 {font-family:sans-serif}</style></head><h1>Backup Google Chat Update Logs</h1><ul>`
     const updates = JSON.parse(fs.readFileSync('updates.json', "utf-8"))
     for (const update of updates.reverse()) {
-      response += `<li><a href="${update.logLink}">${update.releaseDate}.${update.patch}${update.stabilityChar}</a>: ${update.updateName} ${update.stability} (Patch ${update.patch})</li><br>`
+      response += `<li><a target="_blank" href="${update.logLink}">${update.releaseDate}.${update.patch}${update.stabilityChar}</a>: ${update.updateName} ${update.stability} (Patch ${update.patch})</li><br>`
     }
     response += `</ul>`
     res.send(response)
