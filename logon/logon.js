943dfe4220374444bb243569d17192645189c79f
one23four56 <jason@baydq.com>
2021-11-28 16:06:07 -0600

    Format code & enforce DRY

--- logon/logon.js
+++ logon/logon.js
@@ -1,45 +1,57 @@
 const socket = io();
 
-const setHeader = text => document.getElementById("header").innerText = text
+const setHeader = text => id("header").innerText = text
+
+const id = elementID => document.getElementById(elementID)
+
+const q = query => document.querySelector(query)
+
+const badCode = currentForm => {
+    setHeader("Error")
+    q("holder").appendChild(id(currentForm))
+    q("#holder").appendChild(id("reload"))
+    alert("The confirmation code you entered was wrong.")
+}
+
+const pleaseWait = currentForm => {
+    q("holder").appendChild(id(currentForm))
+    setHeader('Please Wait...')
+}
 
 window.alert = (text) => {
-    document.getElementById("alert").style.display = "block"
-    document.getElementById("alert").innerText = text;
+    id("alert").style.display = "block"
+    id("alert").innerText = text;
 }
 
 function setCookie(name, value, ex_days) {
     let ex_date = new Date();
-    ex_date.setTime(ex_date.getTime() + (ex_days*24*60*60*1000));
-    let expires = "expires="+ ex_date.toUTCString();
+    ex_date.setTime(ex_date.getTime() + (ex_days * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + ex_date.toUTCString();
     document.cookie = `${name}=${value};${expires};path=/;SameSite=Lax;Secure;SameParty;}`
 }
 
-document.getElementById("email-form").addEventListener('submit', (event)=>{
+id("email-form").addEventListener('submit', (event) => {
     event.preventDefault()
-    document.querySelector("holder").appendChild(document.getElementById("email-form"))
-    setHeader("Please Wait...")
-    let formdata = new FormData(document.getElementById("email-form"))
+    pleaseWait("email-form")
+    let formdata = new FormData(id("email-form"))
     socket.emit('sign-in', formdata.get('email'), (response) => {
         switch (response) {
             case "bad-email":
                 setHeader("Error")
                 alert('The email you entered is not on our database. Please check for typos and try again using all lowercase letters, and if that does not work, contact me.');
-                document.querySelector("#holder").appendChild(document.getElementById("reload"))
+                q("#holder").appendChild(id("reload"))
                 break;
             case "set-password":
                 setHeader("Set Your Password")
-                document.querySelector("#holder").appendChild(document.getElementById("set-password-form"))
-                document.getElementById("set-password-form").addEventListener('submit', event=>{
+                q("#holder").appendChild(id("set-password-form"))
+                id("set-password-form").addEventListener('submit', event => {
                     event.preventDefault()
-                    const setPasswordFormData = new FormData(document.getElementById('set-password-form'))
-                    if (setPasswordFormData.get("input-password") !== setPasswordFormData.get("confirm-password")) {document.getElementById('confirm-password').setCustomValidity("Passwords do not match.");return}
-                    socket.emit("set-password", {password: setPasswordFormData.get('input-password'), code: setPasswordFormData.get('confirm')}, (setPasswordResponse, data)=>{
+                    const setPasswordFormData = new FormData(id('set-password-form'))
+                    if (setPasswordFormData.get("input-password") !== setPasswordFormData.get("confirm-password")) { id('confirm-password').setCustomValidity("Passwords do not match."); return }
+                    socket.emit("set-password", { password: setPasswordFormData.get('input-password'), code: setPasswordFormData.get('confirm') }, (setPasswordResponse, data) => {
                         switch (setPasswordResponse) {
                             case "bad-code":
-                                setHeader("Error")
-                                document.querySelector("holder").appendChild(document.getElementById("set-password-form"))
-                                document.querySelector("#holder").appendChild(document.getElementById("reload"))
-                                alert("The confirmation code you entered was wrong.")
+                                badCode("set-password-form");
                                 break;
                             case "set-password":
                                 setCookie("email", data.email, 30);
@@ -51,15 +63,16 @@ document.getElementById("email-form").addEventListener('submit', (event)=>{
                 break;
             case "give-password":
                 setHeader("Enter Your Password")
-                document.querySelector("#holder").appendChild(document.getElementById("give-password-form"))
+                q("#holder").appendChild(id("give-password-form"))
                 const passwordSubmitListener = event => {
                     event.preventDefault()
-                    const givePasswordFormData = new FormData(document.getElementById("give-password-form"))
+                    pleaseWait("give-password-form")
+                    const givePasswordFormData = new FormData(id("give-password-form"))
                     socket.emit("give-password", { password: givePasswordFormData.get('password') }, (givePasswordResponse, data) => {
                         if (givePasswordResponse !== "correct") {
                             setHeader("Error")
-                            document.querySelector("holder").appendChild(document.getElementById("give-password-form"))
-                            document.querySelector("#holder").appendChild(document.getElementById("reload"))
+                            q("holder").appendChild(id("give-password-form"))
+                            q("#holder").appendChild(id("reload"))
                             alert("The password you entered was wrong.")
                             return;
                         }
@@ -69,25 +82,21 @@ document.getElementById("email-form").addEventListener('submit', (event)=>{
                         location.reload()
                     })
                 }
-                document.getElementById("give-password-form").addEventListener('submit', passwordSubmitListener)
-                document.getElementById("reset-password").addEventListener('click', e=>{
-                    document.getElementById("give-password-form").removeEventListener('submit', passwordSubmitListener)
-                    document.querySelector("holder").appendChild(document.getElementById("give-password-form"))
-                    setHeader('Please Wait...')
+                id("give-password-form").addEventListener('submit', passwordSubmitListener)
+                id("reset-password").addEventListener('click', e => {
+                    id("give-password-form").removeEventListener('submit', passwordSubmitListener)
+                    pleaseWait("give-password-form")
                     socket.emit('give-password', { type: 'reset' }, passwordResetResponse => {
                         if (passwordResetResponse !== 'sent') return;
-                        document.querySelector("#holder").appendChild(document.getElementById("reset-password-form"))
+                        q("#holder").appendChild(id("reset-password-form"))
                         setHeader("Reset Your Password")
-                        document.getElementById("reset-password-form").addEventListener('submit', event=>{
+                        id("reset-password-form").addEventListener('submit', event => {
                             event.preventDefault()
-                            const resetPasswordFormData = new FormData(document.getElementById("reset-password-form"))
-                            socket.emit('confirm-password-reset', resetPasswordFormData.get("reset-confirm-code"), confirmResetResponse=>{
+                            const resetPasswordFormData = new FormData(id("reset-password-form"))
+                            socket.emit('confirm-password-reset', resetPasswordFormData.get("reset-confirm-code"), confirmResetResponse => {
                                 switch (confirmResetResponse) {
                                     case "bad-code":
-                                        setHeader("Error")
-                                        document.querySelector("holder").appendChild(document.getElementById("reset-password-form"))
-                                        document.querySelector("#holder").appendChild(document.getElementById("reload"))
-                                        alert("The confirmation code you entered was wrong.")
+                                        badCode("reset-password-form");
                                         break;
                                     case "password-reset":
                                         alert("Your password has been reset.")
@@ -98,10 +107,14 @@ document.getElementById("email-form").addEventListener('submit', (event)=>{
                         })
                     })
                 })
+                break;
+            case "send-error":
+                setHeader("Error")
+                alert("Something occurred on our end that interrupted the sending of your email. Please try again and report this to me if it continues.")
         }
     })
-    socket.once('email-sent', _=>{
-        
+    socket.once('email-sent', _ => {
+
     })
 }, {
     once: true
