dc277efd398af85853e51717815aa1614d1d5749
one23four56 <jason@baydq.com>
2022-04-24 13:06:34 -0500

    client/server: add code styling to notices/updates

--- ts/handlers/http/update.ts
+++ ts/handlers/http/update.ts
@@ -2,7 +2,22 @@ import { reqHandlerFunction } from ".";
 import * as fs from "fs";
 import * as path from 'path';
 import * as MarkdownIt from 'markdown-it';
-const markdown = MarkdownIt()
+import hljs from 'highlight.js';
+const markdown = MarkdownIt({
+    highlight: (str, lang) => {
+        if (lang && hljs.getLanguage(lang)) {
+            try {
+                return `<pre class="hljs"><code>${hljs.highlight(str, {
+                    language: lang,
+                    ignoreIllegals: true
+                }).value}</code></pre>`;
+            }
+            catch (__) { }
+        }
+        return `<pre class="hljs"><code>${markdown.utils.escapeHtml(str)}</code></pre>`;
+    },
+    html: true, // so comments work
+})
 
 export const updates: reqHandlerFunction = (req, res) => {
     let response = `<head><title>Backup Google Chat Update Logs</title>`
@@ -18,8 +33,9 @@ export const updateName: reqHandlerFunction = (req, res) => {
     if (req.query.parse === 'true') {
         if (fs.existsSync(path.join(__dirname, '../../../updates', req.params.name))) {
             res.send(
+                '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.0/styles/dark.min.css">' + 
                 "<style>@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap');\n" +
-                "* {font-family: 'Source Sans Pro', sans-serif}\n h1, h2, h3 {font-family: 'Open Sans', sans-serif}" + 
+                "p, li, td, th {font-family: 'Source Sans Pro', sans-serif}\n h1, h2, h3 {font-family: 'Open Sans', sans-serif}" + 
                 "table {width: 100%}</style>" +
                 markdown.render(fs.readFileSync(path.join(__dirname, '../../../updates', req.params.name), 'utf-8')))
         } else res.status(404).send(`The requested file was not found on the server.`)
