eb23c6dd3e3b01bc0f00147f93dbaf5f055347f6
one23four56 <jason@baydq.com>
2022-06-26 19:31:33 -0500

    make script.ts actually run

--- pages/chat/ts/script.ts
+++ pages/chat/ts/script.ts
@@ -1,25 +1,17 @@
 import { alert, confirm, prompt, sideBarAlert } from "./popups"
 import Channel, { View } from './channels'
 import { io } from 'socket.io-client';
-import Dexie from 'dexie';
-import { doInitialMessageLoad, openReactionPicker, getSetting, id, loadSettings, updateStatus, oldToNewConverter } from "./functions";
+import { doInitialMessageLoad, openReactionPicker, getSetting, id, loadSettings } from "./functions";
 import getLoadData from './dataHandler'
-import newMessage from './message'
+import Message from './message'
 import { MessageBar } from "./messageBar";
 
 export const socket = io();
 
-// Dexie.delete('DMDatabase');
-let DMDatabase = new Dexie("DMDatabase");
-
-DMDatabase.version(1).stores({
-    messages: '++key,data'
-})
-
 globalThis.me = await (await fetch('/me')).json()
 
 window.customElements.define('message-holder', View)
-window.customElements.define('message-element', newMessage);
+window.customElements.define('message-element', Message);
 window.customElements.define('message-bar', MessageBar)
 const content = new Channel("content", "Main")
 content.makeMain();
@@ -27,12 +19,10 @@ content.makeMain();
 
 try {
     await loadSettings()
-    await getLoadData()
+    // await getLoadData()
     if (getSetting("misc", "hide-welcome")) document.getElementById("connectdiv-holder").remove();
-    await doInitialMessageLoad()
-} catch (err) {
-    console.error(err)
-}
+    // await doInitialMessageLoad()
+
 
 id("loading").remove()
 
@@ -47,88 +37,70 @@ if (Notification.permission !== 'granted' && Notification.permission !== 'denied
     Notification.requestPermission()
 }
 
-document.getElementById("send").addEventListener('submit', event => {
-    event.preventDefault()
+// document.getElementById("send").addEventListener('submit', event => {
+//     event.preventDefault()
 
-    const formdata = new FormData(id<HTMLFormElement>("send"))
-    id<HTMLInputElement>("text").value = ""
+//     const formdata = new FormData(id<HTMLFormElement>("send"))
+//     id<HTMLInputElement>("text").value = ""
 
-    if (formdata.get("text") === "" && !sessionStorage.getItem("attached-image-url")) return;
+//     if (formdata.get("text") === "" && !sessionStorage.getItem("attached-image-url")) return;
 
-    if (globalThis.messageToEdit) {
-        socket.emit('edit-message', {
-            messageID: globalThis.messageToEdit, 
-            text: formdata.get('text').toString().trim()
-        })
-        delete globalThis.messageToEdit
-        id<HTMLImageElement>('profile-pic-display').src = document.getElementById('profile-pic-display').getAttribute('data-old-src')
-    } else if (globalThis.selectedWebhookId) {
-        if (globalThis.mainChannelId!=="content") {alert("Webhooks are currently not supported in DMs", "Error");return}
-        socket.emit('send-webhook-message', {
-            data: {
-                id: globalThis.selectedWebhookId,
-                text: formdata.get('text').toString().trim(),
-                archive: id<HTMLInputElement>('save-to-archive').checked,
-                image: sessionStorage.getItem("attached-image-url"),
-                replyTo: globalThis.replyTo
-            }
-        });
-    } else {
-        socket.emit('message', {
-            text: formdata.get('text').toString().trim(),
-            archive: id<HTMLFormElement>('save-to-archive').checked,
-            image: sessionStorage.getItem("attached-image-url"),
-            recipient: globalThis.mainChannelId === 'content' ? 'chat' : globalThis.mainChannelId,
-            replyTo: globalThis.replyTo
-        }, data => {
-            data.mute = getSetting('notification', 'sound-send-message')? false : true
-            // if (data.channel && data.channel.to === 'chat') globalThis.channels.content.msg.handle(data);
-            // else {
-            //     globalThis.channels[data.channel.to].msg.handle(data);
-            // }
+//     if (globalThis.messageToEdit) {
+//         socket.emit('edit-message', {
+//             messageID: globalThis.messageToEdit, 
+//             text: formdata.get('text').toString().trim()
+//         })
+//         delete globalThis.messageToEdit
+//         id<HTMLImageElement>('profile-pic-display').src = document.getElementById('profile-pic-display').getAttribute('data-old-src')
+//     } else if (globalThis.selectedWebhookId) {
+//         if (globalThis.mainChannelId!=="content") {alert("Webhooks are currently not supported in DMs", "Error");return}
+//         socket.emit('send-webhook-message', {
+//             data: {
+//                 id: globalThis.selectedWebhookId,
+//                 text: formdata.get('text').toString().trim(),
+//                 archive: id<HTMLInputElement>('save-to-archive').checked,
+//                 image: sessionStorage.getItem("attached-image-url"),
+//                 replyTo: globalThis.replyTo
+//             }
+//         });
+//     } else {
+//         socket.emit('message', {
+//             text: formdata.get('text').toString().trim(),
+//             archive: id<HTMLFormElement>('save-to-archive').checked,
+//             image: sessionStorage.getItem("attached-image-url"),
+//             recipient: globalThis.mainChannelId === 'content' ? 'chat' : globalThis.mainChannelId,
+//             replyTo: globalThis.replyTo
+//         }, data => {
+//             data.mute = getSetting('notification', 'sound-send-message')? false : true
+//             // if (data.channel && data.channel.to === 'chat') globalThis.channels.content.msg.handle(data);
+//             // else {
+//             //     globalThis.channels[data.channel.to].msg.handle(data);
+//             // }
 
-            content.handle(oldToNewConverter(data))
-        })
+//             content.handle(oldToNewConverter(data))
+//         })
 
-    }
-    sessionStorage.removeItem("attached-image-url");
-    document.querySelector<HTMLDivElement>("#attached-image-preview-container").style.display = "none";
-})
+//     }
+//     sessionStorage.removeItem("attached-image-url");
+//     document.querySelector<HTMLDivElement>("#attached-image-preview-container").style.display = "none";
+// })
 
+// socket.on('incoming-message', data => {
+//     // if (data.channel && data.channel.to === 'chat') {
+//     //     globalThis.channels.content.msg.handle(data);
+//     //     messageCount++;
+//     // }
 
-let flash_interval;
-// globalThis.channels.content.msg.secondary = (data) => {
-//     clearInterval(flash_interval)
-//     flash_interval = setInterval(() => {
-//         document.querySelector('#chat-button i').className = "far fa-comments fa-fw"
-//         setTimeout(() => {
-//             document.querySelector('#chat-button i').className = "fas fa-comments fa-fw"
-//         }, 500);
-//     }, 1000);
-//     document.getElementById("chat-button").addEventListener('click', () => {
-//         clearInterval(flash_interval)
-//         document.querySelector('#chat-button i').className = "fas fa-comments fa-fw"
-//     }, {
-//         once: true
-//     })
-// }
+//     // else if (data.channel) {
+//     //     globalThis.channels[data.channel.origin].msg.handle(data);
+//     //     //@ts-expect-error
+//     //     DMDatabase.messages.put({data});
+//     // }
 
-socket.on('incoming-message', data => {
-    // if (data.channel && data.channel.to === 'chat') {
-    //     globalThis.channels.content.msg.handle(data);
-    //     messageCount++;
-    // }
+//     // else {globalThis.channels.content.msg.handle(data);console.warn(`${data.id? `Message #${data.id}` : `Message '${data.text}' (message has no id)`} has no channel. It will be displayed on the main channel.`)};
 
-    // else if (data.channel) {
-    //     globalThis.channels[data.channel.origin].msg.handle(data);
-    //     //@ts-expect-error
-    //     DMDatabase.messages.put({data});
-    // }
-
-    // else {globalThis.channels.content.msg.handle(data);console.warn(`${data.id? `Message #${data.id}` : `Message '${data.text}' (message has no id)`} has no channel. It will be displayed on the main channel.`)};
-
-    content.handle(oldToNewConverter(data))
-})
+//     content.handle(oldToNewConverter(data))
+// })
 
 let alert_timer = null
 socket.on('connection-update', data=>{
@@ -186,61 +158,34 @@ socket.on("forced_disconnect", reason=>{
     alert(`Your connection has been ended by the server, which provided the following reason: \n${reason}`, "Disconnected")
 })
 
-document.querySelector<HTMLInputElement>("#send #text").onpaste = function (event) {
-    //@ts-expect-error
-    let items = (event.clipboardData || event.originalEvent.clipboardData).items;
-    for (let index in items) {
-        let item = items[index];
-        if (item.kind === 'file') {
-            let blob = item.getAsFile();
-            let reader = new FileReader();
-            reader.onload = function (event) {
-                 sessionStorage.setItem("attached-image-url", event.target.result.toString());
+// document.querySelector<HTMLInputElement>("#send #text").onpaste = function (event) {
+//     //@ts-expect-error
+//     let items = (event.clipboardData || event.originalEvent.clipboardData).items;
+//     for (let index in items) {
+//         let item = items[index];
+//         if (item.kind === 'file') {
+//             let blob = item.getAsFile();
+//             let reader = new FileReader();
+//             reader.onload = function (event) {
+//                  sessionStorage.setItem("attached-image-url", event.target.result.toString());
 
-                 if (sessionStorage.getItem("attached-image-url")) {
-                    document.querySelector<HTMLDivElement>("#attached-image-preview-container").style.display = "block";
-                    id<HTMLImageElement>("attached-image-preview").src = sessionStorage.getItem("attached-image-url")
-                    document.getElementById("attached-image-preview").removeAttribute("hidden")
-                }
-            }; 
-            reader.readAsDataURL(blob);
-        }
-    }
-}
+//                  if (sessionStorage.getItem("attached-image-url")) {
+//                     document.querySelector<HTMLDivElement>("#attached-image-preview-container").style.display = "block";
+//                     id<HTMLImageElement>("attached-image-preview").src = sessionStorage.getItem("attached-image-url")
+//                     document.getElementById("attached-image-preview").removeAttribute("hidden")
+//                 }
+//             }; 
+//             reader.readAsDataURL(blob);
+//         }
+//     }
+// }
 
-document.querySelector<HTMLButtonElement>("#attached-image-preview-container #close-button").onclick = _ => {
-    document.querySelector<HTMLDivElement>("#attached-image-preview-container").style.display = "none";
-    if (sessionStorage.getItem("attached-image-url")) {;
-        sessionStorage.removeItem("attached-image-url");
-    }
-}
-
-socket.on("message-deleted", messageID => {
-    let message = document.querySelector(`[data-message-id="${messageID}"]`)
-    const channel = message.parentElement.id
-    message.remove()
-    globalThis.channels[channel].messageObjects = globalThis.channels[channel].messageObjects.filter(item => {
-        if (item.msg === message) {
-            globalThis.channels[channel].messages = globalThis.channels[channel].messages.filter(message => message !== item.data)
-            return false; 
-        } else return true;
-    })
-    globalThis.channels[channel].messageObjects.forEach(message => message.update())
-    messageCount -= 1;
-});
-
-socket.on("message-edited", data => {
-    let message = document.querySelector(`[data-message-id="${data.id}"]`);
-    if (message) {
-        const channel = message.parentElement.id
-        for (let item of globalThis.channels[channel].messageObjects) {
-            if (item.msg !== message) continue; 
-            globalThis.channels[channel].messages[globalThis.channels[channel].messages.indexOf(item.data)] = data;
-            item.data = data;
-        }
-        globalThis.channels[channel].messageObjects.forEach(message => message.update())
-    }
-});
+// document.querySelector<HTMLButtonElement>("#attached-image-preview-container #close-button").onclick = _ => {
+//     document.querySelector<HTMLDivElement>("#attached-image-preview-container").style.display = "none";
+//     if (sessionStorage.getItem("attached-image-url")) {;
+//         sessionStorage.removeItem("attached-image-url");
+//     }
+// }
 
 socket.on("auto-mod-update", data => {
     sideBarAlert(data, 5000, "https://jason-mayer.com/hosted/mod.png")
@@ -272,7 +217,7 @@ document.getElementById("header-logo-image").addEventListener("click", ()=>{
     }
 })
 
-let timeUpdate = setInterval(() => {
+const timeUpdate = setInterval(() => {
     const date =  String(new Date().getDate())
     const ending = !"123".includes(date.slice(-1)) ? 'th' : ['11', '12', '13'].includes(date) ? 'th' : date.slice(-1) === '1' ? 'st' : date.slice(-1) === '2' ? 'nd' : 'rd' //my brain hurts
     const day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][new Date().getDay()]
@@ -280,33 +225,33 @@ let timeUpdate = setInterval(() => {
     document.getElementById("time-disp").innerHTML = `${new Date().toLocaleTimeString()}<br>${day}, ${month} ${date}${ending}`
 }, 500);
 
-setTimeout(_ => {
+// setTimeout(_ => {
 
-    document.getElementById("content").addEventListener('scroll', e => {
-        if (inMessageCoolDown) return;
+//     document.getElementById("content").addEventListener('scroll', e => {
+//         if (inMessageCoolDown) return;
 
-        if (document.getElementById("content").scrollTop > 20) return;
+//         if (document.getElementById("content").scrollTop > 20) return;
 
-        inMessageCoolDown = true;
+//         inMessageCoolDown = true;
 
-        fetch(`/archive.json?reverse=true&start=${messageCount}&count=50`, {
-            headers: {
-                'cookie': document.cookie
-            }
-        }).then(res => {
-            res.json().then(messages => {
-                messageCount += messages.length;
-                for (let data of messages) {
-                    if (data?.tag?.text === "DELETED") continue
-                    data.mute = true
-                    globalThis.channels.content.msg.appendTop(data)
-                }
-                globalThis.channels.content.messageObjects.forEach(message => message.update())
-                setTimeout(_ => { inMessageCoolDown = false }, 500)
-            });
-        })
-    }, { passive: true });
-}, 1000)
+//         fetch(`/archive.json?reverse=true&start=${messageCount}&count=50`, {
+//             headers: {
+//                 'cookie': document.cookie
+//             }
+//         }).then(res => {
+//             res.json().then(messages => {
+//                 messageCount += messages.length;
+//                 for (let data of messages) {
+//                     if (data?.tag?.text === "DELETED") continue
+//                     data.mute = true
+//                     globalThis.channels.content.msg.appendTop(data)
+//                 }
+//                 globalThis.channels.content.messageObjects.forEach(message => message.update())
+//                 setTimeout(_ => { inMessageCoolDown = false }, 500)
+//             });
+//         })
+//     }, { passive: true });
+// }, 1000)
 
 socket.on("forced to disconnect", reason => {
     alert(reason, 'Server-Provided Reason:');
@@ -325,37 +270,38 @@ document.getElementById("profile-picture-holder").addEventListener('click', even
     }
 })
 
-let typingTimer, typingStopped = true;
-document.getElementById("text").addEventListener('input', event => {
-    if (typingStopped)
-        socket.emit('typing start', globalThis.mainChannelId === 'content' ? 'chat' : globalThis.mainChannelId)
+//TODO add this to channels
+// let typingTimer, typingStopped = true;
+// document.getElementById("text").addEventListener('input', event => {
+//     if (typingStopped)
+//         socket.emit('typing start', globalThis.mainChannelId === 'content' ? 'chat' : globalThis.mainChannelId)
 
-    typingStopped = false;
-    clearTimeout(typingTimer);
+//     typingStopped = false;
+//     clearTimeout(typingTimer);
 
-    typingTimer = setTimeout(() => {
-        socket.emit('typing stop', globalThis.mainChannelId === 'content' ? 'chat' : globalThis.mainChannelId)
-        typingStopped = true;
-    }, 500)
-})
+//     typingTimer = setTimeout(() => {
+//         socket.emit('typing stop', globalThis.mainChannelId === 'content' ? 'chat' : globalThis.mainChannelId)
+//         typingStopped = true;
+//     }, 500)
+// })
 
-socket.on('typing', (name, channel) => {
-    let stopTyping;
-    if (channel === 'chat') 
-        stopTyping = globalThis.channels.content.msg.typing(name)
-    else if (channel) 
-        stopTyping = globalThis.channels[channel].msg.typing(name);
+// socket.on('typing', (name, channel) => {
+//     let stopTyping;
+//     if (channel === 'chat') 
+//         stopTyping = globalThis.channels.content.msg.typing(name)
+//     else if (channel) 
+//         stopTyping = globalThis.channels[channel].msg.typing(name);
 
     
-    const endListener = (stopName, stopChannel) => {
-        if (stopName === name && stopChannel === channel) {
-            stopTyping();
-            socket.off('end typing', endListener)
-        }
-    }
+//     const endListener = (stopName, stopChannel) => {
+//         if (stopName === name && stopChannel === channel) {
+//             stopTyping();
+//             socket.off('end typing', endListener)
+//         }
+//     }
     
-    socket.on('end typing', endListener)
-})
+//     socket.on('end typing', endListener)
+// })
 
 socket.on("reaction", (id, message) => {
     let editMessage = document.querySelector(`[data-message-id="${id}"]`);
@@ -466,3 +412,7 @@ id('open-dev-options-button').addEventListener('click', event => {
         }, {once: true})
     }, {once: true})
 })
+
+} catch (err) {
+    console.error(err)
+}
